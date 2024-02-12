e73755621be4c44582e8bb1cb9e49a07400e163f
Ömer Bahrioğlu <bahriogluomer@gmail.com>
2024-02-12 23:36:12 +0300

    closes T1: Project Setup #1

---
+++ src/layout/Main.js
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import PageBody from "./PageBody";
+
+const Main = () => {
+  return (
+    <div className="flex ">
+      <Header />
+      <PageBody />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
